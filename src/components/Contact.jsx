dd59531f5689cf319183a8fc72ae21b66424eaf2
Paywand Mohammed Hussein <73426000+paywand007@users.noreply.github.com>
2023-04-09 14:09:38 +0300

    add bulid folder

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,7 +30,7 @@ const sendEmail = () => {
     <div name='contact' className='w-full h-screen bg-[#00909E] flex justify-center items-center p-4'>
         <form ref={form}
           onSubmit={sendEmail}
-         className='flex flex-col max-w-[600px] w-full'>
+         className='flex flex-col max-w-[600px] w-full '>
             <div className='pb-8 text-center'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#bababa] text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>  Submit the form below or shoot me an email  </p>
